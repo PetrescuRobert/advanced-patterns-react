b26a551509fb64a427c1237429d61ead80427ec7
PetrescuRobert <petrescu.robert69@gmail.com>
2025-05-19 18:52:42 +0300

    Infinite Scroll component for feeds

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { trpc } from "@/trpc.ts";
 import { httpBatchLink } from "@trpc/client";
 import { env } from "@/lib/utils/env.ts";
 import { ExperiencesList } from "@/features/experiences/components/ExperiencesList.tsx";
+import { InfiniteScroll } from "@/features/shared/components/InfiniteScroll.tsx";
 
 export function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -50,12 +51,23 @@ export function App() {
 }
 
 function Index() {
-  const experiencesQuery = trpc.experiences.feed.useQuery({});
+  const experiencesQuery = trpc.experiences.feed.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
 
   return (
-    <ExperiencesList
-      experiences={experiencesQuery.data?.experiences ?? []}
-      isLoading={experiencesQuery.isLoading}
-    />
+    <InfiniteScroll onLoadMore={experiencesQuery.fetchNextPage}>
+      <ExperiencesList
+        experiences={
+          experiencesQuery.data?.pages.flatMap((page) => page.experiences) ?? []
+        }
+        isLoading={
+          experiencesQuery.isLoading || experiencesQuery.isFetchingNextPage
+        }
+      />
+    </InfiniteScroll>
   );
 }
